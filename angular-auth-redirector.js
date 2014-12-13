05ed8007ed346e6d53713cb261f830d6e2a15e25
Can Kayacan <ckayacan@gmail.com>
2014-12-14 00:41:43 +0100

    Last captured path is saved successfully.

--- angular-auth-redirector.js
+++ angular-auth-redirector.js
@@ -21,7 +21,7 @@
         this.setDefaultRedirectPath = function (path) {
             defaultRedirectPath = path;
         };
-        
+
         /**
          * Sets the login path.
          * @param path path to set for login.
@@ -29,22 +29,22 @@
         this.setLoginPath = function (path) {
             loginPath = path;
         };
-        
+
         this.$get = ['$rootScope', '$location', '$timeout', function ($rootScope, $location, $timeout) {
             var redirectPath = '';
             var lastCapturedPath = '';
-            
+
             $rootScope.$on('$routeChangeStart', function(event, next, current) {
-                console.log('route change\n' + next + '\n' + current);
+                lastCapturedPath = $location.url();
             });
-            
-            $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){ 
-                lastCapturedPath = toState.url;
+
+            $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+                lastCapturedPath = $location.url();
             });
-            
+
             return {
                 /**
-                 * Call this function to indicate that authentication was successful and redirect to the 
+                 * Call this function to indicate that authentication was successful and redirect to the
                  * rejected page.
                  * @param data an optional argument to pass on to $broadcast which may be useful for
                  * example if you need to pass through details of the user that was logged in
@@ -54,7 +54,7 @@
                     this.clearRedirectPath();
                     $rootScope.$broadcast('event:auth-loginConfirmed', data);
                 },
-                
+
                 /**
                  * Call this function to indicate that the redirection should not proceed.
                  * @param data an optional argument to pass on to $broadcast.
@@ -63,7 +63,7 @@
                     this.clearRedirectPath();
                     $rootScope.$broadcast('event:auth-loginCancelled', data);
                 },
-                
+
                 /**
                  * Redirects to the login path.
                  */
@@ -72,36 +72,36 @@
                         $timeout(function() { $location.path(loginPath); });
                     }
                 },
-                
+
                 /**
                  * This method is to be called by this module only.
                  * This will be called when 401 is received and will save the current path to be redirected after login.
                  */
                 saveLastCapturedPath: function () {
                     redirectPath = lastCapturedPath;
-                },                
-                
+                },
+
                 /**
                  * Clears the redirect path.
                  */
                 clearRedirectPath: function () {
                     redirectPath = '';
                 },
-                
+
                 /**
                  * Sets a custom redirect path.
                  */
                 setRedirectPath: function (path) {
                     redirectPath = path;
                 },
-                
+
                 /**
                  * Gets the redirect path.
                  */
                 getRedirectPath: function (reason) {
                     return redirectPath === '' ? defaultRedirectPath : redirectPath;
                 },
-                
+
                 /**
                  * Gets the login path.
                  */
